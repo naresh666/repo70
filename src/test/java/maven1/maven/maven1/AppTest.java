41cd3550e65531ddb76401be4e549f263c36796a
naresh666 <nareshan23@gmail.com>
2020-07-30 14:19:10 +0530

    this is 1st commit

---
+++ src/test/java/maven1/maven/maven1/AppTest.java
@@ -0,0 +1,24 @@
+package maven1.maven.maven1;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        Assert.assertEquals(true, true);
+    }
+    @Test
+    public void m1() {
+    	System.out.println("hello world");
+    }
+}
